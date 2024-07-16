711a2f5e44a1c9e5a442eb29069e8268776acd3e
Raj <rajkumarkoramutla926@gmail.com>
2024-07-16 14:27:14 +0530

    Cart feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Payment from './Payment'
+import Cart from './Cart'
 
 const App = () => {
   return (
     <div>
       <Payment />
+      <Cart />
     </div>
   )
 }
